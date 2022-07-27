de37e5d0bd56f7d12efb6ea0d03238d20d40d1f8
Michael Vaganov <mvaganov@hotmail.com>
2022-07-26 23:45:19 -0700

    colorized text works in a test

--- Scripts/ColorizeTMProText.cs
+++ Scripts/ColorizeTMProText.cs
@@ -3,6 +3,7 @@ using NonStandard.Data.Parse;
 using NonStandard.Extension;
 using NonStandard.Ui;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class ColorizeTMProText : MonoBehaviour {
@@ -12,8 +13,11 @@ public class ColorizeTMProText : MonoBehaviour {
 #endif
 	public UnityConsoleOutput consoleOutput;
 	public List<SyntaxColor> _colorList;
+	private Dictionary<string, Color> _colorDictionary = new Dictionary<string, Color>();
+
 	private static Color _unsetColor = new Color(1f, 0f, 1f, 0f);
 	public Color _defaultColor = _unsetColor;
+	public TMP_Text textComponent;
 	[System.Serializable] public class SyntaxColor {
 		public string syntax;
 		public Color color;
@@ -35,6 +39,7 @@ public class ColorizeTMProText : MonoBehaviour {
 	}
 
 	void ColorizeText() {
+		_colorList.ForEach(sc => _colorDictionary[sc.syntax] = sc.color);
 		Tokenizer tok = new Tokenizer();
 		string text = UiText.GetText(gameObject);
 		tok.Tokenize(text);
@@ -43,7 +48,7 @@ public class ColorizeTMProText : MonoBehaviour {
 		//}
 		List<Token> tokens = new List<Token>();
 		tok.GetStandardTokens(tokens);
-		Debug.Log(tokens.Count);
+		//Debug.Log(tokens.Count);
 		Debug.Log(tokens.JoinToString("\n", t => {
 			return t.MetaType + " " + t.ToString();
 		}));
@@ -51,9 +56,45 @@ public class ColorizeTMProText : MonoBehaviour {
 		// generate dictionary of syntax tree color list
 		// calculate syntax tree
 		// go through text and apply color based on color dictionary
+		//SetCharColor(textComponent, 1, new Color[] { Color.green, Color.red, Color.cyan, Color.white, Color.blue, Color.yellow });
+		Color[] color = new Color[1];
+		for (int i = 0; i < tokens.Count; i++) {
+			if (!_colorDictionary.TryGetValue(tokens[i].MetaType, out Color syntaxColor)) {
+				continue;
+			}
+			string t = tokens[i].ToString();
+			color[0] = syntaxColor;
+			SetCharColor(textComponent, tokens[i].index, color, t.Length);
+		}
+		//SetCharColor(textComponent, 1, new Color[] { Color.red, Color.red, Color.red, Color.red });
+		textComponent.UpdateVertexData(TMP_VertexDataUpdateFlags.Colors32);
+		//textComponent.UpdateVertexData(TMP_VertexDataUpdateFlags.Vertices);
+	}
 
-		// TODO what are you doing cheif. didn't you solve this problem before already? isn't there a color grid that is updating?
-		consoleOutput.SetCharColor(1, new Color[] { Color.red });
+	private const char TMProTextOutputTerminator = '\u200B';
+	public static void SetCharColor(TMP_Text textOutput, int index, Color[] colors, int count = -1) {
+		TMP_CharacterInfo[] chars = textOutput.textInfo.characterInfo;
+		if (count < 0) { count = colors.Length; }
+		for (int i = 0; i < count; ++i) {
+			TMP_CharacterInfo cinfo = chars[index++];
+			if (cinfo.character == TMProTextOutputTerminator) break;
+			if (!cinfo.isVisible) continue;
+			int vertexIndex = cinfo.vertexIndex;
+			Color color = colors[i % colors.Length];
+			for (int m = 0; m < 1; ++m) {
+				TMP_MeshInfo meshInfo = textOutput.textInfo.meshInfo[m];
+				if (vertexIndex < 0 || vertexIndex >= meshInfo.colors32.Length) { continue; }
+				SetTmpTextQuadColor(vertexIndex, meshInfo.colors32, color);
+			}
+		}
+	}
+	private static bool SetTmpTextQuadColor(int vertexIndex, Color32[] vertColors, Color color) {
+		if (vertexIndex >= vertColors.Length || vertColors[vertexIndex].EqualRgba(color)) { return false; }
+		vertColors[vertexIndex + 0] = color;
+		vertColors[vertexIndex + 1] = color;
+		vertColors[vertexIndex + 2] = color;
+		vertColors[vertexIndex + 3] = color;
+		return true;
 	}
 
 	void Update() {
